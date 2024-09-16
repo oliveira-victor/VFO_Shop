7e0f9afca6ba65bde1d04804b7f3b8fcec67085f
oliveira-victor <victor.freire.oliveira@gmail.com>
2024-09-16 20:11:15 -0300

    terminada secao dos chaveiros com novas imgs

--- src/Keychains/index.tsx
+++ src/Keychains/index.tsx
@@ -23,13 +23,16 @@ import key11 from '../assets/key11.webp'
 import key12 from '../assets/key12.webp'
 
 import fullkey00 from '../assets/fullkey00.webp';
-import fullkey01 from '../assets/fullkey01.webp';
 import fullkey01a from '../assets/fullkey01a.webp';
+import fullkey01b from '../assets/fullkey01b.webp';
+import fullkey01c from '../assets/fullkey01c.webp';
 import fullkey02 from '../assets/fullkey02.webp';
 import fullkey03 from '../assets/fullkey03.webp';
 import fullkey04 from '../assets/fullkey04.webp';
 import fullkey05 from '../assets/fullkey05.webp';
-import fullkey06 from '../assets/fullkey06.webp';
+import fullkey06a from '../assets/fullkey06a.webp';
+import fullkey06b from '../assets/fullkey06b.webp';
+import fullkey06c from '../assets/fullkey06c.webp';
 import fullkey07 from '../assets/fullkey07.webp';
 import fullkey08 from '../assets/fullkey08.webp';
 import fullkey09 from '../assets/fullkey09.webp';
@@ -45,15 +48,15 @@ const Keychains = () => {
 
     const keychainsData = [
         { thumb: key01a, full: fullkey01a },
-        { thumb: key01b, full: fullkey01 },
-        { thumb: key01c, full: fullkey01 },
+        { thumb: key01b, full: fullkey01b },
+        { thumb: key01c, full: fullkey01c },
         { thumb: key02, full: fullkey02 },
         { thumb: key03, full: fullkey03 },
         { thumb: key04, full: fullkey04 },
         { thumb: key05, full: fullkey05 },
-        { thumb: key06a, full: fullkey06 },
-        { thumb: key06b, full: fullkey06 },
-        { thumb: key06c, full: fullkey06 },
+        { thumb: key06a, full: fullkey06a },
+        { thumb: key06b, full: fullkey06b },
+        { thumb: key06c, full: fullkey06c },
         { thumb: key07, full: fullkey07 },
         { thumb: key08, full: fullkey08 },
         { thumb: key09, full: fullkey09 },
@@ -67,7 +70,7 @@ const Keychains = () => {
             <Hero bgimage={heroKeys} />
             <S.KeysContainer className="container">
                 <S.MainImage>
-                    <img className="display-img" src={displayImage} alt="" />
+                    <img className="display-img" src={displayImage} alt="Miniatura do chaveiro" />
                 </S.MainImage>
                 <S.KeysList>
                     {keychainsData.map((img, index) => (
