1a78f259550f4930ce7fc93969631cdd42b93451
oliveira-victor <victor.freire.oliveira@gmail.com>
2024-09-17 22:26:36 -0300

    feat

--- src/containers/Prints/index.tsx
+++ src/containers/Prints/index.tsx
@@ -5,15 +5,27 @@ import heroPrints from '../../assets/hero-prints.webp'
 import fullprint00 from '../../assets/fullprint00.webp'
 
 import thumb01 from '../../assets/thumb-darkwoods.webp'
+import thumb02 from '../../assets/thumb-alice.webp'
+import thumb03 from '../../assets/thumb-paperboat.webp'
+import thumb04 from '../../assets/thumb-flyingwhale.webp'
+import thumb05 from '../../assets/thumb-harp.webp'
 
 import fullprint01 from '../../assets/fullprint01.webp'
+import fullprint02 from '../../assets/fullprint02.webp'
+import fullprint03 from '../../assets/fullprint03.webp'
+import fullprint04 from '../../assets/fullprint04.webp'
+import fullprint05 from '../../assets/fullprint05.webp'
 
 const Prints = () => {
 
     const displayImage = fullprint00
 
     const printsData = [
-        { thumb: thumb01, full: fullprint01 }
+        { thumb: thumb01, full: fullprint01 },
+        { thumb: thumb02, full: fullprint02 },
+        { thumb: thumb03, full: fullprint03 },
+        { thumb: thumb04, full: fullprint04 },
+        { thumb: thumb05, full: fullprint05 }
     ]
 
     return (
