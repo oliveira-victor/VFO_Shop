c2e048bc6096e46c60442ab5c71e0eee922d9d24
oliveira-victor <victor.freire.oliveira@gmail.com>
2024-09-18 21:57:37 -0300

    feat: cards page done

--- src/containers/Cards/index.tsx
+++ src/containers/Cards/index.tsx
@@ -1,12 +1,46 @@
 import Hero from "../../components/Hero"
+import Gallery from "../../components/Gallery"
 
 import heroCard from '../../assets/hero-card.webp'
+import fullcard00 from '../../assets/fullcard00.webp'
+
+import thumb01 from '../../assets/thumb-flyingwhale.webp'
+import thumb02 from '../../assets/thumb-darkwoods.webp'
+import thumb03 from '../../assets/thumb-harp.webp'
+import thumb04 from '../../assets/thumb-moon.webp'
+import thumb05 from '../../assets/thumb-paperboat.webp'
+import thumb06 from '../../assets/thumb-hairsea.webp'
+import thumb07 from '../../assets/thumb-magictree.webp'
+
+import fullprint01 from '../../assets/fullcard01.webp'
+import fullprint02 from '../../assets/fullcard02.webp'
+import fullprint03 from '../../assets/fullcard03.webp'
+import fullprint04 from '../../assets/fullcard04.webp'
+import fullprint05 from '../../assets/fullcard05.webp'
+import fullprint06 from '../../assets/fullcard06.webp'
+import fullprint07 from '../../assets/fullcard07.webp'
 
 const Cards = () => {
+
+    const displayImage = fullcard00
+
+    const printsData = [
+        { thumb: thumb01, full: fullprint01 },
+        { thumb: thumb02, full: fullprint02 },
+        { thumb: thumb03, full: fullprint03 },
+        { thumb: thumb04, full: fullprint04 },
+        { thumb: thumb05, full: fullprint05 },
+        { thumb: thumb06, full: fullprint06 },
+        { thumb: thumb07, full: fullprint07 }
+    ]
+
     return (
         <>
             <Hero bgimage={heroCard} />
-            <h2 style={{ textAlign: 'center' }}>Em breve.</h2>
+            <Gallery
+                displayImage={displayImage}
+                thumbs={printsData}
+            />
         </>
     )
 }
--- src/containers/Prints/index.tsx
+++ src/containers/Prints/index.tsx
@@ -5,16 +5,18 @@ import heroPrints from '../../assets/hero-prints.webp'
 import fullprint00 from '../../assets/fullprint00.webp'
 
 import thumb01 from '../../assets/thumb-darkwoods.webp'
-import thumb02 from '../../assets/thumb-alice.webp'
+import thumb02 from '../../assets/thumb-hairsea.webp'
 import thumb03 from '../../assets/thumb-paperboat.webp'
 import thumb04 from '../../assets/thumb-flyingwhale.webp'
 import thumb05 from '../../assets/thumb-harp.webp'
+import thumb06 from '../../assets/thumb-moon.webp'
 
 import fullprint01 from '../../assets/fullprint01.webp'
 import fullprint02 from '../../assets/fullprint02.webp'
 import fullprint03 from '../../assets/fullprint03.webp'
 import fullprint04 from '../../assets/fullprint04.webp'
 import fullprint05 from '../../assets/fullprint05.webp'
+import fullprint06 from '../../assets/fullprint06.webp'
 
 const Prints = () => {
 
@@ -25,7 +27,8 @@ const Prints = () => {
         { thumb: thumb02, full: fullprint02 },
         { thumb: thumb03, full: fullprint03 },
         { thumb: thumb04, full: fullprint04 },
-        { thumb: thumb05, full: fullprint05 }
+        { thumb: thumb05, full: fullprint05 },
+        { thumb: thumb06, full: fullprint06 }
     ]
 
     return (
